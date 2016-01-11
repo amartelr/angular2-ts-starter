4a0167e23c2fd1f49e4d5bcf31795aeed0a733e7
amartelr <alfredo.martel@gmail.com>
2016-01-11 16:45:45 +0000

    Disabling AppComponent Component

--- app/boot.ts
+++ app/boot.ts
@@ -2,10 +2,10 @@
 import {bootstrap}    from 'angular2/platform/browser'
 
 // load our our components
-import {AppComponent} from './app.component'
+//import {AppComponent} from './app.component'
 import {HelloWorldComponent} from './hello-world'
 
 //This will instantiates our components
 
-bootstrap(AppComponent);
+//bootstrap(AppComponent);
 bootstrap(HelloWorldComponent);
