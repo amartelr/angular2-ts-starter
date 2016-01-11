b7ac00390f9f86f38be0fb6f95501b5280516c88
amartelr <alfredo.martel@gmail.com>
2016-01-11 20:46:19 +0000

    HeroComponent

--- app/boot.ts
+++ app/boot.ts
@@ -4,8 +4,10 @@ import {bootstrap}    from 'angular2/platform/browser'
 // load our our components
 import {NavComponent} from './components/nav.component'
 import {HelloWorldComponent} from './components/hello-world'
+import {HeroComponent} from './components/hero.component'
 
 //This will instantiates our components
 
 bootstrap(NavComponent);
 bootstrap(HelloWorldComponent);
+bootstrap(HeroComponent);
